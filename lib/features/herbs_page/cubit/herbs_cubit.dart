a090a7d01d5fc4d2fcc53904e82aa3c7aa39674d
Iwona Rudzinska <rudzinska.iwona96@gmail.com>
2022-05-17 10:32:33 +0200

    Add flutter_bloc to pubspec.yaml

---
+++ lib/features/herbs_page/cubit/herbs_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+part 'herbs_state.dart';
+
+class HerbsCubit extends Cubit<HerbsState> {
+  HerbsCubit() : super(HerbsState(documents: const [],),);
+}
---
+++ lib/features/herbs_page/cubit/herbs_state.dart
@@ -0,0 +1,7 @@
+part of 'herbs_cubit.dart';
+
+
+class HerbsState {
+  List<QueryDocumentSnapshot<Map<String, dynamic>>> documents;
+  HerbsState({required this.documents});
+}
