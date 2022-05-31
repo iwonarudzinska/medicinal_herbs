84aeea413aa633324642e1899da89342cd4fdc77
Iwona Rudzinska <rudzinska.iwona96@gmail.com>
2022-05-31 17:57:19 +0200

    Add new title

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Medicinal Herbs',
       theme: ThemeData(
         scaffoldBackgroundColor: const Color.fromARGB(255, 191, 223, 192),
         inputDecorationTheme: InputDecorationTheme(
